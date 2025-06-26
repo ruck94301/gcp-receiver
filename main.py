036d7af02565f1de3d86a9a1e7023e7c677d9cf1
ruck94301 <John.Ruckstuhl@gmail.com>
2025-06-26 08:49:29 -0700

    Generalize main.py to run local (for testing) or on GCP.

--- main.py
+++ main.py
@@ -1,28 +1,48 @@
+import logging
+import os
+
 import cherrypy
 import wsgiref.handlers
+
+logging.basicConfig(level=logging.DEBUG)
+logging.info('%s: %r', '__name__', __name__)
+
 
 class Root:
     @cherrypy.expose
     def index(self):
         return "Hello, CherryPy from Google App Engine!"
 
-if True or __name__ == '__main__':
 
-    # This ?? from search -- cherry py app wsgi main
-    # Mount the CherryPy application to get a WSGI callable
-    #1 myapp = cherrypy.tree.mount(Root())
-    # You can then use this 'application' callable with any WSGI server.
-    # For example, if using Gunicorn:
-    # gunicorn -w 4 your_module:myapp
-    #2
-    cherrypy.tree.mount(Root())
-    myapp = cherrypy.tree
+def is_running_on_gcp():
+    """Determine if the application is running on Google Cloud Platform."""
+    return 'GOOGLE_CLOUD_PROJECT' in os.environ
 
-    # Use the following configuration for App Engine.
-    # It's important to bind to 0.0.0.0 to listen on all interfaces
-    # and to set the port to 8080 as required by App Engine.
-    cherrypy.config.update({'server.socket_host': '0.0.0.0',
-                            'server.socket_port': 8080})
 
-# if __name__ == '__main__':
-#     cherrypy.quickstart(Root())
+# This ?? from search -- cherry py app wsgi main
+# Mount the CherryPy application to get a WSGI callable
+#1 myapp = cherrypy.tree.mount(Root())
+# You can then use this 'application' callable with any WSGI server.
+# For example, if using Gunicorn:
+# gunicorn -w 4 your_module:myapp
+#2
+cherrypy.tree.mount(Root())
+myapp = cherrypy.tree
+
+# Use the following configuration for App Engine.
+# It's important to bind to 0.0.0.0 to listen on all interfaces
+# and to set the port to 8080 as required by App Engine.
+cherrypy.config.update({'server.socket_host': '0.0.0.0',
+                        'server.socket_port': 8080})
+
+if is_running_on_gcp():
+    print("Running on Google Cloud")
+    pass
+
+elif __name__ == '__main__':
+    print("Not running on Google Cloud")
+    cherrypy.quickstart(Root())
+
+else:
+    print("Not running on Google Cloud")
+    logging.error('unexpected')
